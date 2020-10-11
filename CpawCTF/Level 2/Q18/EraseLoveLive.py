420622a834a173b2f01ff3e39daec0ae795b1b70
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-10-11 21:28:01 +0900

    [add]Q18 finish

---
+++ CpawCTF/Level 2/Q18/EraseLoveLive.py
@@ -0,0 +1,33 @@
+import os
+
+def erase_love_live(S:str) -> str:
+    S = S.replace('lovelive!', '')
+    return S
+
+def get_flag(S:str) -> str:
+    tmp = []
+    #大文字を集める
+    for i in range(len(S)):
+        if S[i].isupper() or S[i] == '{' or S[i] == '}':
+            tmp.append(S[i])
+    #３文字の頭のアルファベットのみを抽出
+    flag = []
+    for i in range(0,len(tmp),3):
+        if tmp[i].isupper():
+            flag.append(tmp[i].lower())
+        else:
+            flag.append(tmp[i])
+    return ''.join(flag)
+
+def main() -> str:
+    path = os.getcwd()
+    path = path+'/CpawCTF/Level 2/Q18/misc100'
+    with open(path) as m:
+        S = m.read()
+        not_love_live = erase_love_live(S)
+        flag = get_flag(not_love_live)
+        return flag
+
+if __name__ == "__main__":
+    result = main()
+    print(result)
---
+++ CpawCTF/Level 2/Q18/README.md
@@ -0,0 +1,31 @@
+## 問題文
+Q18.[Forensic]leaf in forest
+
+このファイルの中にはフラグがあります。探してください。
+フラグはすべて小文字です！
+
+file
+
+## 解答
+ダウンロードしたファイルは拡張子がなかったためfileでタイプを調査
+
+```
+% file misc100
+misc100: pcap capture file, microsecond ts (little-endian) - version 0.0 (linktype#1768711542, capture length 1869357413)
+```
+
+pcapタイプであることが判明した。
+
+Wiresharkで開くかを検討していたら、テキストファイルとしてメモ帳やvsコードで展開ができた
+
+中身は大量の「lovelive!」の文字列となっていたためpythonのスクリプトで「lovelive!」を消した文字列を取得
+
+アルファベットの大文字が３つ不自然に続いている文字列てあったが、
+頭の文字列が「CCCPPPAAAWWW{{{」となっていたためcpaw{と変換できると考えられる
+
+pyhtonのスクリプトを修正してflagを取得
+
+cpaw{mgrep}
+
+## コード 
+参照：EraseLoveLive.py
